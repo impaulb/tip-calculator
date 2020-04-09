a09f784585ccee11d4a9e6996f27bc0ce8e2baa6
Paul <59670248+impaulb@users.noreply.github.com>
2020-04-09 14:51:40 -0700

    Seamless typing submit

--- assets/js/tip.js
+++ assets/js/tip.js
@@ -1,18 +1,16 @@
-$("#submit").click(function(){
-  $("#error").text("");
-  $("#tipAmount").text("");
-  if($("#amount").val() == ""){
-    $("#error").text("Bill amount undefined!");
-  } else if ($("#service").val() == "blank"){
-    $("#error").text("Service level undefined!");
-  } else if ($("#party").val() == ""){
-    $("#error").text("Party number undefined!");
-  } else {
-    var billAmount = $("#amount").val();
-    var serviceLevel = $("#service").val();
-    var peopleInParty = $("#party").val();
-    var tipPercent = 0.10 + (serviceLevel * 0.0275) + ((peopleInParty - 2) * 0.01);
-    var tipAmount = billAmount * tipPercent;
-    $("#tipAmount").text("You should tip: " + tipPercent.toFixed(2) * 100 + "% or $" + tipAmount.toFixed(2) + ".");
-  }
+var amount = $("#amount");
+var service = $("#service");
+var party = $("#party");
+
+var userInput = $(".user");
+
+userInput.on("input", function(){
+if(amount.val() != "" && service.val() != "blank" && party.val() != ""){
+  var billAmount = $("#amount").val();
+  var serviceLevel = $("#service").val();
+  var peopleInParty = $("#party").val();
+  var tipPercent = 0.10 + (serviceLevel * 0.025) + ((peopleInParty - 2) * 0.002);
+  var tipAmount = billAmount * tipPercent;
+  $("#tipAmount").text("You should tip: " + tipPercent.toFixed(2) * 100 + "% or $" + tipAmount.toFixed(2) + ".");
+}
 });
